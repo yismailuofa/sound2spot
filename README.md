c75308a21c621f897d19bd04ecb109ee25f30555
Youssef Ismail <yismail@ualberta.ca>
2023-05-03 13:55:34 -0600

    cmnd line

--- README.md
+++ README.md
@@ -2,11 +2,11 @@ A script that converts a soundcloud tracklist to a Spotify playlist.
 
 ## Usage
 
+```python
+python3 main.py <URL>
+```
+
 1. Install the requirements
 2. Change the local variables in the script to your own
 3. Create a .env with your spotify keys
 4. Run the script
-
-## Todo
-
-- [ ] Add support for any url
--- main.py
+++ main.py
@@ -2,25 +2,30 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import os
 from dotenv import load_dotenv
+import sys
 
 from soundcloud import getTitleAndTracks
 
 # Replace with your own Spotify client ID and secret
-redirect_uri = 'http://localhost:8000'
+redirect_uri = "http://localhost:8000"
 
 load_dotenv()
 
 # Authentication
-auth_manager = SpotifyOAuth(client_id=os.getenv('SPOTIFY_CLIENT_ID'),  # Change this to your own
-                            # Change this to your own
-                            client_secret=os.getenv('SPOTIFY_CLIENT_SECRET'),
-                            redirect_uri=redirect_uri,
-                            scope='playlist-modify-public')
+auth_manager = SpotifyOAuth(
+    client_id=os.getenv("SPOTIFY_CLIENT_ID"),  # Change this to your own
+    # Change this to your own
+    client_secret=os.getenv("SPOTIFY_CLIENT_SECRET"),
+    redirect_uri=redirect_uri,
+    scope="playlist-modify-public",
+)
 
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
-# Change this to your own
-URL = "https://soundcloud.com/g0homemasterpiece/after-dark-episode-31"
+# Passed in from the command line
+URL = sys.argv[1] if len(sys.argv) > 1 else None
+
+assert URL is not None, "Please provide a URL"
 
 title, song_list = getTitleAndTracks(URL)
 
@@ -30,11 +35,10 @@ if title is None or song_list is None:
 
 
 def search_track(track_name):
-
-    results = sp.search(q=track_name, type='track', limit=1)
-    if results['tracks']['items']:
+    results = sp.search(q=track_name, type="track", limit=1)
+    if results["tracks"]["items"]:
         print(f"Track found: {track_name}")
-        return results['tracks']['items'][0]['id']
+        return results["tracks"]["items"][0]["id"]
     else:
         print(f"Track not found: {track_name}")
         return None
@@ -46,13 +50,12 @@ track_ids = [id for id in track_ids if id is not None]
 
 
 # Get the user's ID
-user_id = sp.me()['id']
+user_id = sp.me()["id"]
 
 # Create the playlist
-playlist = sp.user_playlist_create(
-    user_id, title, public=True, description='')
+playlist = sp.user_playlist_create(user_id, title, public=True, description="")
 
 # Add the tracks to the playlist
-sp.playlist_add_items(playlist['id'], track_ids)
+sp.playlist_add_items(playlist["id"], track_ids)
 
 print(f"Playlist '{title}' created successfully!")
